61b1f500f774112a9d3446c8e9f5437dd0fe9686
Nancy Fulda <nancy_fulda@yahoo.com>
2018-04-02 18:37:06 -0600

    Added word embeddings to encoder/decoder model in encoder_decoder_fasttext.py. Successfully overfits, but it's slooooooow...

---
+++ dataset_fasttext.py
@@ -0,0 +1,121 @@
+from collections import Counter
+import numpy as np
+import codecs
+import torch
+from torch.autograd import Variable
+
+
+class Dataset:
+
+
+    def __init__(self, filename="data/ijcnlp_dailydialog/dialogues_text.txt"):
+        self.generate_vocab(filename)
+        self.generate_conversations(filename)
+        self.create_batches()
+        self.reset_batch_pointer()
+
+
+    def generate_vocab(self, filename):
+        with codecs.open(filename, "r", encoding='utf-8') as f:
+            data = f.read()
+        counter = Counter(data)
+        count_pairs = sorted(counter.items(), key=lambda x: -x[1])
+        self.chars, _ = zip(*count_pairs)
+        self.chars = list(self.chars)
+
+        self.chars.append("SOS ")
+        self.chars.append(" EOS")
+
+        self.vocab_size = len(self.chars)
+        self.vocab = dict(zip(self.chars, range(len(self.chars))))
+        self.EOS_index = self.vocab[" EOS"]
+        self.SOS_index = self.vocab["SOS "]
+
+    
+    def generate_conversations(self, filename):
+        with open(filename, "r") as file:
+            text_content = file.readlines()
+            
+        self.conversations = []
+        for line_no in range(len(text_content)):
+            text = text_content[line_no].split("__eou__")
+            text = [t.strip() for t in text if t.strip() != ""]
+
+            for turn in range(len(text) - 1):
+                self.conversations.append((text[turn], text[turn+1]))
+
+
+    def create_batches(self):
+        self.encoder_turns = []
+        self.decoder_turns = []
+        self.encoded_conversations = []
+        longest_possible_sequence = 200
+        for convo in self.conversations:
+            if len(convo[0]) > longest_possible_sequence or \
+                    len(convo[1]) > longest_possible_sequence:
+                continue
+
+            #encoder_turn = list(map(self.vocab.get, convo[0]))
+            encoder_turn = convo[0].lower().split(' ')
+            #encoder_turn.insert(0, self.SOS_index)
+            #encoder_turn.append(self.EOS_index)
+            encoder_turn.insert(0, 'SOS')
+            encoder_turn.append('EOS')
+            encoder_turn = np.array(encoder_turn)
+
+            #decoder_turn = list(map(self.vocab.get, convo[1]))
+            decoder_turn = convo[1].lower().split(' ')
+            #decoder_turn.insert(0, self.SOS_index)
+            #decoder_turn.append(self.EOS_index)
+            decoder_turn.insert(0, 'SOS')
+            decoder_turn.append('EOS')
+            decoder_turn = np.array(decoder_turn)
+
+            self.encoded_conversations.append((encoder_turn, decoder_turn))
+            self.encoder_turns.append(encoder_turn)
+            self.decoder_turns.append(decoder_turn)
+
+
+    def next_batch(self):
+        encoder, decoder = self.encoder_turns[self.pointer], self.decoder_turns[self.pointer]
+        self.pointer += 1
+        if self.pointer >= len(self.encoder_turns):
+            self.reset_batch_pointer()
+        return encoder, decoder
+
+
+    def reset_batch_pointer(self):
+        self.pointer = 0
+
+
+    def to_onehot(self, x, long_type=False):
+        onehot_stack = torch.zeros((len(x), self.vocab_size))
+        onehot_stack[np.array(range(len(x))), x] = 1
+        if long_type:
+            onehot_stack = onehot_stack.type(torch.LongTensor)
+        return Variable(onehot_stack)
+
+
+    def to_phrase(self, x):
+        return "".join([self.chars[x[i]] for i in range(len(x))])
+    
+    def to_array(self, x):
+        #return np.array([self.chars.index(char) for char in x])
+        return np.array(x.lower().split(' '))
+
+    def size(self):
+        return len(self.encoder_turns)
+
+
+
+if __name__ == "__main__":
+    dataset = Dataset()
+    x, y = dataset.next_batch()
+
+    # print the indicies
+    print(x)
+    print(y)
+
+    # print the chars
+    print("".join([dataset.chars[x[i]] for i in range(len(x))]))
+    print("".join([dataset.chars[y[i]] for i in range(len(y))]))
---
+++ encoder_decoder_fasttext.py
@@ -0,0 +1,280 @@
+import numpy as np
+import sys
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torch import optim
+from torch.autograd import Variable
+
+#from dataset import Dataset
+from dataset_fasttext import Dataset
+import fasttext
+torch.set_printoptions(profile="short")
+
+#
+# ===============================================
+# ===============================================
+# ===============================================
+#
+# Globals
+
+# init dataset
+dataset = Dataset()
+
+# Sizes
+#VOCAB_SIZE = dataset.vocab_size
+VOCAB_SIZE = 50002
+HIDDEN_SIZE = 300 #size of fasttext vectors
+#HIDDEN_SIZE = 256
+LEARNING_RATE = .001
+MAX_LENGTH = 256
+USE_CUDA = False
+
+#
+# ===============================================
+# ===============================================
+# ===============================================
+#
+# RNNs
+
+class EncoderRNN(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers):
+        super(EncoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        ftext = fasttext.FastText()
+        ftextTensor = torch.FloatTensor(ftext.vectors)
+        #self.embedding = nn.Embedding.from_pretrained(ftextTensor)
+        self.embedding.weight.data.copy_(ftextTensor)
+
+        #self.fasttext_embedding = fasttext.FastText()
+        self.gru = nn.GRU(hidden_size, hidden_size, num_layers)
+
+    def forward(self, input, hidden):
+        #print("Input: ", input)
+        #print("Input data: ", input.data)
+
+        #get the fasttext embedding of the vector
+        #embedded = np.squeeze(self.fasttext_embedding.vectors[input.data])
+        embedded = self.embedding(input).view(self.num_layers, 1, -1)
+        #print("Embedded len: ", len(embedded))
+        #print("Embedded: ", embedded)
+        output = embedded
+        output, hidden = self.gru(output, hidden)
+        return output, hidden
+
+    def init_hidden(self):
+        result = Variable(torch.zeros(1, 1, self.hidden_size))
+        if USE_CUDA:
+            return result.cuda()
+        else:
+            return result
+
+
+class DecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size, num_layers):
+        super(DecoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size, num_layers)
+        self.out = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        output = self.embedding(input).view(self.num_layers, 1, -1)
+        output = F.relu(output)
+        output, hidden = self.gru(output, hidden)
+        output = self.softmax(self.out(output[0]))
+        return output, hidden
+
+    def init_hidden(self):
+        result = Variable(torch.zeros(1, 1, self.hidden_size))
+        if USE_CUDA:
+            return result.cuda()
+        else:
+            return result
+
+#
+# ===============================================
+# ===============================================
+# ===============================================
+#
+# Training function
+
+def train(input_variable,
+          target_variable,
+          encoder,
+          decoder,
+          encoder_optimizer,
+          decoder_optimizer,
+          criterion,
+          epoch,
+          convo_i,
+          teacher_forcing_ratio=.5,
+          max_length=MAX_LENGTH):
+
+    ftext = fasttext.FastText()
+
+    encoder_hidden = encoder.init_hidden()
+
+    encoder_optimizer.zero_grad()
+    decoder_optimizer.zero_grad()
+
+    input_length = len(input_variable)
+    target_length = len(target_variable)
+
+    encoder_outputs = Variable(torch.zeros(max_length, encoder.hidden_size))
+    encoder_outputs = encoder_outputs.cuda() if USE_CUDA else encoder_outputs
+
+    decoder_outputs = -np.ones((max_length))
+
+    loss = 0
+
+    for ei in range(input_length):
+        encoder_output, encoder_hidden = encoder(
+            input_variable[ei], encoder_hidden)
+        encoder_outputs[ei] = encoder_output[0][0]
+
+    decoder_input = Variable(torch.LongTensor([[dataset.SOS_index]]))
+    decoder_input = decoder_input.cuda() if USE_CUDA else decoder_input
+
+    decoder_hidden = encoder_hidden
+
+    use_teacher_forcing = True if np.random.random() < teacher_forcing_ratio else False
+
+    if use_teacher_forcing:
+        # Teacher forcing: Feed the target as the next input
+        for di in range(target_length):
+            decoder_output, decoder_hidden = decoder(
+                decoder_input, decoder_hidden)
+            loss += criterion(decoder_output, target_variable[di])
+            decoder_input = target_variable[di]  # Teacher forcing
+
+            topv, topi = decoder_output.data.topk(1)
+            ni = topi[0][0]
+            decoder_outputs[di] = ni
+            # out_char = decoder_output.data.numpy()[0]
+            # out_char = out_char / np.sum(out_char)
+            # decoder_outputs[di] = np.random.choice(range(VOCAB_SIZE), p=out_char)
+
+    else:
+        # Without teacher forcing: use its own predictions as the next input
+        for di in range(target_length):
+            decoder_output, decoder_hidden = decoder(
+                decoder_input, decoder_hidden)
+            topv, topi = decoder_output.data.topk(1)
+            ni = topi[0][0]
+
+            decoder_input = Variable(torch.LongTensor([[ni]]))
+            decoder_input = decoder_input.cuda() if USE_CUDA else decoder_input
+
+            loss += criterion(decoder_output, target_variable[di])
+
+            decoder_outputs[di] = ni
+            # out_char = decoder_output.data.numpy()[0]
+            # out_char = out_char / np.sum(out_char)
+            # decoder_outputs[di] = np.random.choice(range(VOCAB_SIZE), p=out_char)
+
+            if ni == dataset.EOS_index:
+                break
+
+
+    loss.backward()
+    encoder_optimizer.step()
+    decoder_optimizer.step()
+
+    # prepare offer
+    input_variable = input_variable.view(-1)
+    offer = input_variable.data.numpy()
+
+    # prepare answer
+    target_variable = target_variable.view(-1)
+    answer = target_variable.data.numpy()
+
+    # prepare rnn
+    for i in range(len(decoder_outputs)):
+        if decoder_outputs[i] == -1:
+            break
+    formated_decoder_outputs = decoder_outputs[:i]
+    rnn_response = list(map(int, formated_decoder_outputs))
+
+    if convo_i%1 == 0:
+        print("\n---------------------------")
+        print("Epoch: {}, Step: {}".format(epoch, convo_i))
+        print("Loss:   {}".format(loss.data[0] / target_length))
+        print("---------------------------\n")
+        #print("Offer: ", dataset.to_phrase(offer))
+        #print("Answer:", dataset.to_phrase(answer))
+        #print("RNN:", dataset.to_phrase(rnn_response))
+        print(type(offer))
+        print(len(offer))
+        sys.stdout.flush()
+        print("Offer: ", ' '.join(ftext.get_words_from_indices(offer)))
+        print("Answer:", ' '.join(ftext.get_words_from_indices(answer)))
+        print("RNN:", ' '.join(ftext.get_words_from_indices(rnn_response)))
+
+
+#
+# ===============================================
+# ===============================================
+# ===============================================
+#
+# Training loop
+
+
+# define rnns
+encoder_rnn = EncoderRNN(input_size=VOCAB_SIZE,
+                         hidden_size=HIDDEN_SIZE,
+                         num_layers=1)
+
+decoder_rnn = DecoderRNN(hidden_size=HIDDEN_SIZE,
+                         output_size=VOCAB_SIZE,
+                         num_layers=1)
+
+# loss
+criterion = nn.NLLLoss()
+encoder_optimizer = optim.SGD(encoder_rnn.parameters(), lr=LEARNING_RATE)
+decoder_optimizer = optim.SGD(decoder_rnn.parameters(), lr=LEARNING_RATE)
+
+# main conversation loop
+# x, y = dataset.next_batch()
+
+f = fasttext.FastText()
+for epoch in range(30):
+    for convo_i in range(dataset.size()):
+        # translation loop vars
+        x, y = dataset.next_batch()
+        x = np.reshape(x, (-1, 1))
+        y = np.reshape(y, (-1, 1))
+
+        #print("x ", x)
+        #print("y ", y)
+
+        #HACK to check overfit
+        y = ['SOS','this','is','a','test','.','EOS']
+
+        x = f.get_indices(x)
+        y = f.get_indices(y)
+        #print("x ", x)
+        #print("y ", y)
+
+        x2 = Variable(torch.LongTensor(x))
+        y2 = Variable(torch.LongTensor(y))
+        #print("x2 ", x2)
+        #print("y2 ", y2)
+
+        train(Variable(torch.LongTensor(x)),
+            Variable(torch.LongTensor(y)),
+            encoder_rnn,
+            decoder_rnn,
+            encoder_optimizer,
+            decoder_optimizer,
+            criterion,
+            epoch,
+            convo_i)
