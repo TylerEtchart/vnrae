b82998c9ad9713fbbc74c77ea64578f10d3a16bc
Nancy Fulda <nancy_fulda@yahoo.com>
2018-04-10 20:24:08 -0600

    vrae gru now uses FastText embeddings. Overfits within 400 training steps.

--- vrae_gru.py
+++ vrae_gru.py
@@ -11,9 +11,8 @@ import pyro.distributions as dist
 from pyro.infer import SVI
 from pyro.util import ng_zeros, ng_ones
 
-#from dataset import Dataset
-from dataset_fasttext import Dataset
 import fasttext
+from dataset_fasttext import Dataset
 
 #
 # ===============================================
@@ -27,18 +26,14 @@ import fasttext
 dataset = Dataset()
 
 # Sizes
-#VOCAB_SIZE = dataset.vocab_size
 VOCAB_SIZE = 50002
-#ENCODER_HIDDEN_SIZE = 256
-ENCODER_HIDDEN_SIZE = 300
-#DECODER_HIDDEN_SIZE = 512
+ENCODER_HIDDEN_SIZE = 300 
 DECODER_HIDDEN_SIZE = 600
-#Z_DIMENSION = 256
-Z_DIMENSION = 300
+Z_DIMENSION =300 
 LEARNING_RATE = .0001
-#MAX_LENGTH = 256
 MAX_LENGTH = 300
-USE_CUDA = False
+NUM_LAYERS_FOR_RNNS = 1
+USE_CUDA = True
 
 
 #
@@ -49,37 +44,44 @@ USE_CUDA = False
 # RNNs
 
 class EncoderRNN(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers):
+    def __init__(self, input_size, hidden_size, num_layers, ftext):
         super(EncoderRNN, self).__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
 
-        ftext = fasttext.FastText()
-        ftextTensor = torch.FloatTensor(ftext.vectors)
-        self.embedding = nn.Embedding(input_size, hidden_size)
-        self.embedding.weight.data.copy_(ftextTensor)
-        #self.embedding.weight.requires_grad = False
-
+        self.fc = nn.Linear(input_size, num_layers*hidden_size)
         self.rnn = nn.GRU(hidden_size, hidden_size, num_layers=num_layers)
+        
+        self.fc.weight.data.copy_(torch.FloatTensor(ftext.vectors).view_as(self.fc.weight.data))
+        #self.fc.weight.requires_grad = False
 
-    def forward(self, input, hidden):
+
+    def forward(self, input_var, hidden):
         if USE_CUDA:
-            input = input.cuda()
-        embedded = self.embedding(input).view(self.num_layers, 1, -1)
-        output = embedded
-        output, hidden = self.rnn(output, hidden)
+            input_var = input_var.cuda()
+        embedded = self.fc(input_var).view(self.num_layers, 1, -1)
+        output, hidden = self.rnn(embedded, hidden)
+
+        if type(self.fc.weight.grad) == type(None):
+            print("EncoderRNN fc gradiants are none")
+
+        if type(self.rnn.weight_ih_l0.grad) == type(None):
+            print("EncoderRNN IH gradiants are none")
+        if type(self.rnn.weight_hh_l0.grad) == type(None):
+            print("EncoderRNN HH gradiants are none")
+
         return output, hidden
 
     def init_hidden_lstm(self):
-        result = (Variable(torch.zeros(1, 1, self.hidden_size)),
-                  Variable(torch.zeros(1, 1, self.hidden_size)))
+        result = (Variable(torch.zeros(self.num_layers, 1, self.hidden_size)),
+                  Variable(torch.zeros(self.num_layers, 1, self.hidden_size)))
         if USE_CUDA:
             return result.cuda()
         else:
             return result
 
     def init_hidden_gru(self):
-        result = Variable(torch.zeros(1, 1, self.hidden_size))
+        result = Variable(torch.zeros(self.num_layers, 1, self.hidden_size))
         if USE_CUDA:
             return result.cuda()
         else:
@@ -92,30 +94,43 @@ class DecoderRNN(nn.Module):
         self.hidden_size = hidden_size
         self.num_layers = num_layers
 
-        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.embedding = nn.Linear(output_size, num_layers*hidden_size)
         self.rnn = nn.GRU(hidden_size, hidden_size, num_layers=num_layers)
         self.out = nn.Linear(hidden_size, output_size)
-        self.softmax = nn.LogSoftmax(dim=1)
+        self.softmax = nn.Softmax(dim=1)
 
-    def forward(self, input, hidden):
+    def forward(self, input_var, hidden):
         if USE_CUDA:
-            input = input.cuda()
-        output = self.embedding(input).view(self.num_layers, 1, -1)
+            input_var = input_var.cuda()
+
+        hidden = hidden.view(self.num_layers, 1, self.hidden_size)
+
+        output = self.embedding(input_var).view(self.num_layers, 1, -1)
         output = F.relu(output)
         output, hidden = self.rnn(output, hidden)
         output = self.softmax(self.out(output[0]))
+
+        if type(self.embedding.weight.grad) == type(None):
+            print("DecoderRNN embedding weights are none")
+        if type(self.rnn.weight_ih_l0.grad) == type(None):
+            print("DecoderRNN IH weights are none")
+        if type(self.rnn.weight_hh_l0.grad) == type(None):
+            print("DecoderRNN HH weights are none")
+        # else:
+        #     print("DecoderRNN HH weight sum ", torch.sum(self.rnn.weight_hh_l0.data))
+
         return output, hidden
 
     def init_hidden_lstm(self):
-        result = (Variable(torch.zeros(1, 1, self.hidden_size)),
-                  Variable(torch.zeros(1, 1, self.hidden_size)))
+        result = (Variable(torch.zeros(self.num_layers, 1, self.hidden_size)),
+                  Variable(torch.zeros(self.num_layers, 1, self.hidden_size)))
         if USE_CUDA:
             return result.cuda()
         else:
             return result
 
     def init_hidden_gru(self):
-        result = Variable(torch.zeros(1, 1, self.hidden_size))
+        result = Variable(torch.zeros(self.num_layers, 1, self.hidden_size))
         if USE_CUDA:
             return result.cuda()
         else:
@@ -141,6 +156,12 @@ class EncoderDense(nn.Module):
         x = x.view(-1, self.hidden_dim)
         z_mu = self.fc_mu(x)
         z_sigma = torch.exp(self.fc_sig(x))
+
+        if type(self.fc_mu.weight.grad) == type(None):
+            print("EncoderDense mu grad is none")
+        if type(self.fc_sig.weight.grad) == type(None):
+            print("EncoderDense sig grad is none")
+
         return z_mu, z_sigma
 
 
@@ -152,7 +173,12 @@ class DecoderDense(nn.Module):
 
     def forward(self, z):
         hidden = self.fc(z)
-        hidden = hidden.view(1, 1, self.hidden_dim)
+        
+        if type(self.fc.weight.grad) == type(None):
+            print("DecoderDense grad is none")
+        # else:
+        #     print("DecoderDense weight sum", torch.sum(self.fc.weight.data))
+
         return hidden
 
 
@@ -174,26 +200,30 @@ class VRAE(nn.Module):
                  z_dim,
                  decoder_hidden_dim,
                  max_length,
+                 num_layers_for_rnns,
                  use_cuda=False):
         super(VRAE, self).__init__()
-        # define rnns
 
+        #fastText (for creating output phrases)
         self.ftext = fasttext.FastText()
 
+        # define rnns
+        self.num_layers = num_layers_for_rnns
         self.encoder_rnn = EncoderRNN(input_size=vocab_dim,
-                                 hidden_size=encoder_hidden_dim,
-                                 num_layers=1)
+                                      hidden_size=encoder_hidden_dim,
+                                      num_layers=self.num_layers,
+                                      ftext = self.ftext)
 
         self.decoder_rnn = DecoderRNN(hidden_size=decoder_hidden_dim,
-                                 output_size=vocab_dim,
-                                 num_layers=1)
+                                      output_size=vocab_dim,
+                                      num_layers=self.num_layers)
 
         # define dense modules
         self.encoder_dense = EncoderDense(hidden_dim=encoder_hidden_dim,
-                                     z_dim=z_dim)
+                                          z_dim=z_dim)
 
         self.decoder_dense = DecoderDense(z_dim=z_dim,
-                                     hidden_dim=decoder_hidden_dim)
+                                          hidden_dim=decoder_hidden_dim)
 
         if use_cuda:
             # calling cuda() here will put all the parameters of
@@ -205,86 +235,78 @@ class VRAE(nn.Module):
         self.max_length = max_length
 
 
-    def model(self, input_variable, target_variable):
+    def model(self, input_variable, target_variable, step):
         # register PyTorch module `decoder` with Pyro
         pyro.module("decoder_dense", self.decoder_dense)
         pyro.module("decoder_rnn", self.decoder_rnn)
 
         # setup hyperparameters for prior p(z)
         # the type_as ensures we get CUDA Tensors if x is on gpu
-        z_mu = ng_zeros([1, self.z_dim], type_as=x.data)
-        z_sigma = ng_ones([1, self.z_dim], type_as=x.data)
+        z_mu = ng_zeros([self.num_layers, self.z_dim], type_as=target_variable.data)
+        z_sigma = ng_ones([self.num_layers, self.z_dim], type_as=target_variable.data)
 
         # sample from prior
         # (value will be sampled by guide when computing the ELBO)
-        z_mu = z_mu.float()
-        z_sigma = z_sigma.float()
         z = pyro.sample("latent", dist.normal, z_mu, z_sigma)
 
         # init vars
-        decoder_hidden = self.decoder_dense(z)
-        target_length = len(target_variable)
-        decoder_input = Variable(torch.LongTensor([[self.dataset.SOS_index]]))
-        decoder_input = decoder_input.cuda() if USE_CUDA else decoder_input
-        decoder_outputs = -np.ones((target_length))
+        target_length = target_variable.shape[0]
 
-        # Teacher forcing
+        decoder_input = dataset.to_onehot([[self.ftext.SOS_index]])
+        decoder_input = decoder_input.cuda() if USE_CUDA else decoder_input
+
+        decoder_outputs = np.ones((target_length))
+        decoder_hidden = self.decoder_dense(z)
+
+        # # Teacher forcing
         for di in range(target_length):
             decoder_output, decoder_hidden = self.decoder_rnn(
                 decoder_input, decoder_hidden)
-            decoder_input = target_variable[di]  
+            decoder_input = target_variable[di]
 
-            topv, topi = decoder_output.data.topk(1)
-            ni = topi[0][0]
-            decoder_outputs[di] = ni
-            # out_char = decoder_output.data.numpy()[0]
-            # out_char = out_char / np.sum(out_char)
-            # decoder_outputs[di] = np.random.choice(range(VOCAB_SIZE), p=out_char)
+            if self.use_cuda:
+                decoder_outputs[di] = np.argmax(decoder_output.cpu().data.numpy())
+            else:
+                decoder_outputs[di] = np.argmax(decoder_output.data.numpy())
+
+            pyro.observe("obs_{}".format(di), dist.bernoulli, target_variable[di], decoder_output[0])
 
         # ----------------------------------------------------------------
         # prepare offer
-        input_variable = input_variable.view(-1)
         if self.use_cuda:
-            offer = input_variable.cpu().data.numpy()
+            offer = np.argmax(input_variable.cpu().data.numpy(), axis=1).astype(int)
         else:
-            offer = input_variable.data.numpy()
+            offer = np.argmax(input_variable.data.numpy(), axis=1).astype(int)
 
         # prepare answer
-        target_variable = target_variable.view(-1)
         if self.use_cuda:
-            answer = target_variable.cpu().data.numpy()
+            answer = np.argmax(target_variable.cpu().data.numpy(), axis=1).astype(int)
         else:
-            answer = target_variable.data.numpy()
+            answer = np.argmax(target_variable.data.numpy(), axis=1).astype(int)
 
         # prepare rnn
         rnn_response = list(map(int, decoder_outputs))
         
         # print output
-        
-        #print("---------------------------")
-        #print("Offer: ", dataset.to_phrase(offer))
-        #print("Answer:", self.dataset.to_phrase(answer))
-        #print("RNN:", self.dataset.to_phrase(rnn_response))
-
-        print("---------------------------")
-        print("Offer: ", ' '.join(self.ftext.get_words_from_indices(offer)))
-        print("Answer:", ' '.join(self.ftext.get_words_from_indices(answer)))
-        print("RNN:   ", ' '.join(self.ftext.get_words_from_indices(rnn_response)))
+        if step % 10 == 0:
+            print("---------------------------")
+            #print("Offer: ", dataset.to_phrase(offer))
+            #print("Answer:", self.dataset.to_phrase(answer))
+            #print("RNN:", self.dataset.to_phrase(rnn_response))
+            print("Offer: ", ' '.join(self.ftext.get_words_from_indices(offer)))
+            print("Answer:", ' '.join(self.ftext.get_words_from_indices(answer)))
+            print("RNN:", ' '.join(self.ftext.get_words_from_indices(rnn_response)))
         # ----------------------------------------------------------------
 
-        decoder_outputs = Variable(torch.Tensor(decoder_outputs))
-        target_variable = target_variable.float()
-        pyro.observe("obs", dist.bernoulli, target_variable, decoder_outputs)
 
-
-    def guide(self, input_variable, target_variable):
+    def guide(self, input_variable, target_variable, step):
         # register PyTorch module `encoder` with Pyro
         pyro.module("encoder_dense", self.encoder_dense)
         pyro.module("encoder_rnn", self.encoder_rnn)
 
         # init vars
-        input_length = len(input_variable)
-        encoder_outputs = Variable(torch.zeros(self.max_length, self.encoder_rnn.hidden_size))
+        input_length = input_variable.shape[0]
+        encoder_outputs = Variable(torch.zeros(input_length, self.encoder_rnn.hidden_size))
         encoder_outputs = encoder_outputs.cuda() if USE_CUDA else encoder_outputs
         encoder_hidden = self.encoder_rnn.init_hidden_gru()
 
@@ -318,15 +340,14 @@ vrae = VRAE(dataset,
             Z_DIMENSION,
             DECODER_HIDDEN_SIZE,
             MAX_LENGTH,
+            NUM_LAYERS_FOR_RNNS,
             USE_CUDA)
 optimizer = optim.Adam({"lr": LEARNING_RATE})
-#encoder_params = filter(lambda p: p.requires_grad, encoder_rnn.parameters())
-#optimizer = optim.Adam(encoder_params, lr=LEARNING_RATE)
 svi = SVI(vrae.model, vrae.guide, optimizer, loss="ELBO")
 
 f=fasttext.FastText()
 
-for epoch in range(num_epochs):
+for epoch in range(30):
     print("Start epoch!")
     # initialize loss accumulator
     epoch_loss = 0.
@@ -335,22 +356,25 @@ for epoch in range(num_epochs):
     for convo_i in range(dataset.size()):
         x, y = dataset.next_batch()
 
-        #HACK to check overfit
+        #HACK for overfitting
         y = ['SOS', 'this', 'is', 'a', 'test', '.', 'EOS']
 
         x = f.get_indices(x)
         y = f.get_indices(y)
 
-        x = np.reshape(x, (-1, 1))
-        y = np.reshape(y, (-1, 1))
-
-        x = Variable(torch.LongTensor(x))
-        y = Variable(torch.LongTensor(y))
-
+        x = dataset.to_onehot(x, long_type=False)
+        y = dataset.to_onehot(y, long_type=False)
+        
         # do ELBO gradient and accumulate loss
-        loss = svi.step(x, y)
+        if USE_CUDA:
+            loss = svi.step(x.cuda(), y.cuda(), convo_i)
+        else:
+            loss = svi.step(x, y, convo_i)
         epoch_loss += loss
-        print("Epoch: {}, Step: {}, NLL: {}".format(epoch, convo_i, loss))
-        print("---------------------------\n")
 
-    print("Trained epoch: {}, epoch loss: {}".format(epoch, epoch_loss))
+        # print loss
+        if convo_i % 10 == 0:
+            print("Epoch: {}, Step: {}, NLL: {}".format(epoch, convo_i, loss))
+            print("---------------------------\n")
+
+    print("\n\nTrained epoch: {}, epoch loss: {}\n\n".format(epoch, epoch_loss))
